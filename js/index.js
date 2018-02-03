efea918f708966e9418cc6afbc0e19c298e427bd
arjun-lalwani <arjun11@uw.edu>
2018-02-03 02:09:37 -0800

    fix images

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let images = {
     'easy': ['cake.gif', 'caterpillar.gif', 'christmas-tree.gif', 'duck.gif', 'elephant.png'],
     'medium': ['boat.gif', 'frog.gif', 'rocket.gif', 'train.gif', 'whale.gif'],
@@ -6,31 +8,42 @@ let images = {
 
 let state = {
     'color': 'blue',
-    'level': 'easy'
+    'level': 'easy',
+    'mouseState': ''
 }
 
-let colors = $('#dropdown-colors').click(function (event) {
+$(function () {
+    adjustCanvasSize();
+});
+
+
+$('#dropdown-colors').click(function (event) {
     state.color = event.target.innerText;
+    changeColorStatus();
 });
 
-let levels = $('#dropdown-levels').click(function (event) {
-    console.log(event.target.innerText.toLowerCase());
+$('#dropdown-levels').click(function (event) {
     state.level = event.target.innerText.toLowerCase();
+    changeLevelStatus();
 });
 
+$('#red-btn, #green-btn, #blue-btn, #purple-btn, #gold-btn, #brown-btn').click(function (event) {
+    state.color = event.target.innerText;
+    changeColorStatus();
+});
+
+$('#easy-btn, #medium-btn, #hard-btn').click(function (event) {
+    state.level = event.target.innerText.toLowerCase();
+    changeLevelStatus();
+});
 
 $('#generate-img').click(function (event) {
 
     // select canvas
     let ctx = $('canvas')[0].getContext('2d');
-    var lod = state.level.toLowerCase;
-    if (state.level === 'medium') {
-        lod = 'medium';
-    } else if (state.level === 'hard') {
-        lod = 'hard';
-    }
-    // get random index to access from array for each level
+    var lod = state.level;
 
+    // get random index to access from array for each level
     let randomIndex = Math.floor(Math.random() * images[lod].length);
 
     // generate path for image
@@ -40,32 +53,108 @@ $('#generate-img').click(function (event) {
     img.onload = function () {
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-        let ctxWidth = $('canvas')[0].scrollWidth; // does not get width from CSS
-        let ctxHeight = $('canvas')[0].scrollHeight; // does not get height from CSS
+        let ctxWidth = canvas.width; // does not get width from CSS
+        let ctxHeight = canvas.height; // does not get height from CSS
         var imgWidth = 167.5;
         var imgHeight = 205;
         var startX = 90;
         var startY = 40;
 
         // scale image differently depending on canvas width
-        if (ctxWidth === 508) {
-            imgWidth = 230;
-            imgHeight = 281;
-            startX = 60;
-            startY = 10;
-        } else if (ctxWidth === 688) {
-            imgWidth = 240;
-            imgHeight = 294;
-            startX = 50;
-            startY = 0;
-        } else if (ctxWidth === 948) {
-            imgWidth = 240;
-            imgHeight = 294;
-            startX = 50;
-            startY = 0;
+        if (ctxWidth === 550) {
+            imgWidth = 260;
+            imgHeight = 318;
+            startX = 160;
+            startY = 40;
+        } else if (ctxWidth === 700) {
+            imgWidth = 350;
+            imgHeight = 428;
+            startX = 190;
+            startY = 30;
+        } else if (ctxWidth === 850) {
+            imgWidth = 400;
+            imgHeight = 489;
+            startX = 210;
+            startY = 30;
+        } else if (ctxWidth === 950) {
+            imgWidth = 450;
+            imgHeight = 550;
+            startX = 240;
+            startY = 30;
         }
         ctx.drawImage(img, startX, startY, imgWidth, imgHeight);
+
     }
     img.src = src;
 });
 
+
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+
+$(window).resize(function () {
+    adjustCanvasSize();
+});
+
+function adjustCanvasSize() {
+    let canvas = document.querySelector('canvas');
+    let window_width = $(window).width();
+
+    if (window_width < 576) {
+        canvas.width = 350;
+        canvas.height = 300;
+    } else if (window_width >= 576 && window_width < 768) {
+        canvas.width = 550;
+        canvas.height = 400;
+    } else if (window_width >= 768 && window_width < 992) {
+        canvas.width = 700;
+        canvas.height = 500;
+    } else if (window_width >= 992 && window_width < 1200) {
+        canvas.width = 850;
+        canvas.height = 600;
+    } else if (window_width >= 1200) {
+        canvas.width = 950;
+        canvas.height = 700;
+    }
+}
+
+canvas.addEventListener('mousedown', function (event) {
+    state.mouseState = 'mouseDown';
+    ctx.strokeStyle = state.color;
+    let canvasOffset = $('canvas').offset();
+    ctx.moveTo((event.clientX - canvasOffset.left), (event.clientY - canvasOffset.top));
+
+    // begin path
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', function (event) {
+    if (state.mouseState === 'mouseDown') {
+        ctx.lineWidth = 2;
+        let canvasOffset = $('canvas').offset();
+        ctx.lineTo((event.clientX - canvasOffset.left), (event.clientY - canvasOffset.top));
+        ctx.stroke();
+    }
+});
+
+
+canvas.addEventListener('mouseup', function () {
+    state.mouseState = "mouseUp";
+    ctx.closePath();
+});
+
+
+function changeColorStatus() {
+
+    $('#status-color').text(capitalize(state.color));
+}
+
+function changeLevelStatus() {
+    $('#status-level').text(capitalize(state.level));
+}
+
+// found on stack overflow
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
